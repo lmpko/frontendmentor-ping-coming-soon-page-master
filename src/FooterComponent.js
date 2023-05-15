448efced3e7ef37f8b8c1498d7771993c48864ad
lmpko <lmpkoo@gmail.com>
2023-05-15 10:12:23 +0200

    social menu component created , html structure for this component added and component added to footer

--- src/FooterComponent.js
+++ src/FooterComponent.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import SocialMenuComponent from './SocialMenuComponent'
 
 const FooterComponent = () => {
   return (
     <div>
+      <SocialMenuComponent/>
       <p>&copy; Copyright Ping. All rights reserved.</p>
     </div>
   )
---
+++ src/SocialMenuComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SocialMenuComponent = () => {
+  return (
+    <ul>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+    </ul>
+  )
+}
+
+export default SocialMenuComponent
