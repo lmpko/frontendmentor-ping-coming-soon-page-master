13bbfe69c82a4d57d756479dc3e77cac00059009
lmpko <lmpkoo@gmail.com>
2023-05-15 10:14:15 +0200

    image added to layout component

--- src/Layout.js
+++ src/Layout.js
@@ -2,12 +2,14 @@ import React from 'react'
 import OverviewComponent from './OverviewComponent'
 import FormComponent from './FormComponent'
 import FooterComponent from './FooterComponent'
+import preview from "/images/illustration-dashboard.png"
 
 const Layout = () => {
   return (
     <>
       <OverviewComponent/>
       <FormComponent />
+      <img src={preview} alt="preview" />
       <FooterComponent/>
     </>
   )
