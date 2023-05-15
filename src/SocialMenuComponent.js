0b5b2830825a034dbc3f1c9dc1e8dd9689c089a6
lmpko <lmpkoo@gmail.com>
2023-05-15 15:45:19 +0200

    aria labe ladded to link

--- src/SocialMenuComponent.js
+++ src/SocialMenuComponent.js
@@ -4,9 +4,9 @@ import {ImFacebook, ImTwitter,ImInstagram} from "react-icons/im"
 const SocialMenuComponent = () => {
   return (
     <ul>
-        <li><a href="#"><ImFacebook/></a></li>
-        <li><a href="#"><ImTwitter/></a></li>
-        <li><a href="#"><ImInstagram/></a></li>
+        <li><a href="#" aria-label='facebook'><ImFacebook/></a></li>
+        <li><a href="#" aria-label='twitter'><ImTwitter/></a></li>
+        <li><a href="#" aria-label='instagram'><ImInstagram/></a></li>
     </ul>
   )
 }
