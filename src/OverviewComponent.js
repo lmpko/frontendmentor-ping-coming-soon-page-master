c3fe37da559a3fe517ec67d597691d4bde9ad9a8
lmpko <lmpkoo@gmail.com>
2023-05-15 10:07:38 +0200

    overview component html structure added

--- src/OverviewComponent.js
+++ src/OverviewComponent.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import logo from "/images/logo.svg"
 
 const OverviewComponent = () => {
   return (
-    <div>OverviewComponent</div>
+    <section>
+      <img src={logo} alt=""/>
+      <h1>We are launching <span>soon!</span></h1>
+      <p>Subscribe and get notified</p>
+    </section>
   )
 }
 
