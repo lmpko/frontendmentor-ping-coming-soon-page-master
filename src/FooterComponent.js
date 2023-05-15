f3e60970c3e12dd848f632a2430678f550dbeacc
lmpko <lmpkoo@gmail.com>
2023-05-15 11:28:10 +0200

    react icons added, social menu css added

--- src/FooterComponent.js
+++ src/FooterComponent.js
@@ -3,7 +3,7 @@ import SocialMenuComponent from './SocialMenuComponent'
 
 const FooterComponent = () => {
   return (
-    <div>
+    <div className='footer-container'>
       <SocialMenuComponent/>
       <p>&copy; Copyright Ping. All rights reserved.</p>
     </div>
--- src/SocialMenuComponent.js
+++ src/SocialMenuComponent.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import {ImFacebook, ImTwitter,ImInstagram} from "react-icons/im"
 
 const SocialMenuComponent = () => {
   return (
     <ul>
-        <li><a href=""></a></li>
-        <li><a href=""></a></li>
-        <li><a href=""></a></li>
+        <li><a href=""><ImFacebook/></a></li>
+        <li><a href=""><ImTwitter/></a></li>
+        <li><a href=""><ImInstagram/></a></li>
     </ul>
   )
 }
